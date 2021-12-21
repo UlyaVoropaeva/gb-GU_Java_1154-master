a0e221452778621db9602248005986c69afd0ced
Lenovo-PC <Ulya2811@yandex.ru>
2021-12-21 23:43:08 +0300

    lesson_12

---
+++ gb-GU_Java_1154-master/application/src/main/java/ru/gb/mall/configuration/CustomAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package ru.gb.mall.configuration;
+
+
+//@Component
+public class CustomAccessDeniedHandler /* implements AccessDeniedHandler*/ {
+/*
+    private static Logger LOG = LoggerFactory.getLogger(MyAccessDeniedHandler.class);
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException exc) throws IOException, ServletException {
+
+        Authentication auth
+                = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            LOG.warn("User: " + auth.getName()
+                    + " attempted to access the protected URL: "
+                    + request.getRequestURI());
+        }
+
+        response.sendRedirect(request.getContextPath() + "/accessDenied");
+    }
+*/
+}
---
+++ gb-GU_Java_1154-master/application/src/main/java/ru/gb/mall/inventory/mail/message/AttachmentMailMessage.java
@@ -0,0 +1,11 @@
+package ru.gb.mall.inventory.mail.message;
+
+public record AttachmentMailMessage(
+        String from,
+
+        String to,
+        String subject,
+        String text,
+        EmailAttachment attachment
+)  {
+}
